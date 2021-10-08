947c5a2636a9610e35922f3d85a4c89835b928bf
Maryery Vargas Moreno <maryeryvargas@gmail.com>
2021-10-08 14:57:07 +0100

    Color Switcher

---
+++ app.js
@@ -0,0 +1,33 @@
+"use strict";
+var hexValues = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+];
+var btn = document.getElementById('btn');
+var color = document.querySelector('.color');
+btn.addEventListener('click', function () {
+    var hexColor = '#';
+    for (var i = 0; i < 6; i++) {
+        console.log(getRandomNumber());
+        hexColor += hexValues[getRandomNumber()];
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+function getRandomNumber() {
+    return Math.floor(Math.random() * hexValues.length);
+}
